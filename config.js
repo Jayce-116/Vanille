58fd20859305d9f91284bdf29dec1405f002de4f
Jayce-116 <jessemirembeet@gmail.com>
2026-03-17 15:24:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    // Replace 'YOUR_USERNAME' with your PythonAnywhere username
-    API_BASE_URL: 'https://YOUR_USERNAME.pythonanywhere.com'
+    
+    API_BASE_URL: 'https://Jayce.pythonanywhere.com'
 };
